4d51512243ed0232cb56de78e009da9ef622bd20
djye <djye2010@gmail.com>
2025-07-02 16:22:39 +0800

    update config

--- src/sodex_api/config.py
+++ src/sodex_api/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Config:
     SODEX_API_KEY = os.getenv("SODEX_API_KEY", "1234567890")
     SODEX_SECRET_KEY = os.getenv("SODEX_SECRET_KEY", "1234567890")
-    SODEX_BASE_URL = 'https://sodex-openapi.sosovalue.io'
-    SODEX_WS_URL = 'wss://sodex-openapi.sosovalue.io'
-    SODEX_BASE_URL_EXT = 'https://test-vex.sosovalue.io'
+    SODEX_BASE_URL = 'http://gateway-jp.test.sodex.dev'
+    SODEX_WS_URL = 'ws://gateway-jp.test.sodex.dev'
+    SODEX_BASE_URL_EXT = 'http://gateway-jp.test.sodex.dev'
